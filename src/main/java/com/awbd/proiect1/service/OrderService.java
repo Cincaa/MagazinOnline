cbefb9952d1aaf3b25d69271f534ab0c96b34c87
Larisa-Cristina <larisacristina99@gmail.com>
2023-05-19 21:08:24 +0300

    Fix error

--- src/main/java/com/awbd/proiect1/service/OrderService.java
+++ src/main/java/com/awbd/proiect1/service/OrderService.java
@@ -9,6 +9,7 @@ import com.awbd.proiect1.repository.CartRepository;
 import com.awbd.proiect1.repository.OrderRepository;
 import com.awbd.proiect1.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -16,6 +17,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class OrderService {
 
     @Autowired
